5b90d7ede4cc36089bef9e2412315d7f4ac6e940
josecharrisdev <josecharrisdev@gmail.com>
2026-03-08 23:07:54 -0500

    Se crea proyecto para practicas APIS

---
+++ Apis/src/main/java/co/jose/curso/springboot/apis/Apis/ApisApplication.java
@@ -0,0 +1,13 @@
+package co.jose.curso.springboot.apis.Apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisApplication.class, args);
+	}
+
+}
