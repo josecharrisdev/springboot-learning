c44abda35cb7b7929d8f4f5f08df1abe8d590a57
josecharrisdev <josecharrisdev@gmail.com>
2026-03-21 17:30:19 -0500

    Se realizan los métodos restantes del CRUD

--- Students/src/main/java/co/charris/jose/students/controller/TeacherController.java
+++ Students/src/main/java/co/charris/jose/students/controller/TeacherController.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +27,42 @@ public class TeacherController {
 	
 	@PostMapping("/insertTeacher")
 	public ResponseEntity<?> insertTeacher(@RequestBody Teacher teacher){
-		this.listTeacher.add(teacher);
-		return ResponseEntity.status(HttpStatus.CREATED).body("Se creó el registro de profesor.");
+		if(this.getTeacherById(teacher.getId()).getStatusCode().equals(HttpStatus.OK)) {
+			this.listTeacher.add(teacher);
+			return ResponseEntity.status(HttpStatus.CREATED).body("Se creó el registro de profesor.");
+		}
+		return ResponseEntity.notFound().build();
 	}
+	
+	
+	@GetMapping("/getTeacherById")
+	public ResponseEntity<?> getTeacherById(@PathVariable String id){
+		return this.listTeacher.stream().filter(teacher -> teacher.getId().equals(id))
+				.findFirst().map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> updateTeacher(@RequestBody Teacher newTeacher){
+		for(Teacher teacher: this.listTeacher) {
+			if(teacher.getId().equals(newTeacher.getId())) {
+				teacher.setName(newTeacher.getName());
+				teacher.setCode(newTeacher.getCode());
+				teacher.setAge(newTeacher.getAge());
+				return ResponseEntity.noContent().build();
+			}
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> deleteTeacher(@PathVariable String id) {
+		boolean eliminar = this.listTeacher.removeIf((t) -> t.getId().equals(id));
+		if(eliminar) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	
+	
 }
